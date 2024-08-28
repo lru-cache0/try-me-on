2ced2a4029b87c7600048aa4aab4e284ad915197
lru-cache0 <lrucache0@gmail.com>
2024-08-28 13:02:20 -0500

    dress

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -105,7 +105,7 @@ export default async function handler(
     },
     body: JSON.stringify({
       version: versionId,
-      input: { garm_img, human_img, garment_des: "dress"},
+      input: { garm_img, human_img, garment_des: "dress", category: "dress" },
     }),
   });
 
